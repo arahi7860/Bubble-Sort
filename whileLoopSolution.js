365be3199c33ec1673da60a0a810c285bc7322bf
Tara Solbrig <tsolbrig87@gmail.com>
2020-07-27 10:32:24 -0400

    Update whileLoopSolution.js
    had totalLoops += 1 on the wrong line

--- whileLoopSolution.js
+++ whileLoopSolution.js
@@ -1,5 +1,5 @@
 // random array:
-let arr = [2, 10, 20, 8, 6, 7, 5, 3, 0, 9]
+let arr = [2, 10, 20, 8, 6, 7, 5, 3, 0, 9];
 
 // worst case, O(n^2):
 // let arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -15,7 +15,7 @@ const bubbleSort = (arr) => {
   // these are just to see what's happening
   let totalChecks = 0;
   let totalLoops = 0;
-  // do {some function} while (swap is true, 
+  // do {some function} while (swap is true,
   // meaning a swap happened in that pass)
   do {
     // set swap to false at the beginning of each loop
@@ -23,14 +23,14 @@ const bubbleSort = (arr) => {
     console.log("Current", arr);
     // for loop to loop through the array, one index at a time
     for (let i = 0; i < arr.length; i++) {
-      // if the first number is less than the second number, 
+      // if the first number is less than the second number,
       // they need to be swapped
       if (arr[i] > arr[i + 1]) {
         // set a temp variable to hold the first value
         let temp = arr[i];
         // set the first value to the second value
         arr[i] = arr[i + 1];
-        // set the second value to the old value of the first 
+        // set the second value to the old value of the first
         // (stored in temp)
         arr[i + 1] = temp;
         // a swap just happened, set swap to true
@@ -38,13 +38,21 @@ const bubbleSort = (arr) => {
       }
       // increment our checks and loops
       totalChecks += 1;
-      totalLoops += 1;
     }
+    totalLoops += 1;
   } while (swap);
   // once it makes it through the loop for a full iteration
   // without hitting the if statement, and thus without a swap
   // we can log our final array (and our checks and loops)
-  console.log("Final", arr, "completed in", totalChecks, "checks, and", totalLoops, "loops");
+  console.log(
+    "Final",
+    arr,
+    "completed in",
+    totalChecks,
+    "checks, and",
+    totalLoops,
+    "loops"
+  );
 };
 
 bubbleSort(arr);
